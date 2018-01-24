a9581d368569f52c09bddffbf06d990b7d9a85db
craigwardman <craigwardman@hotmail.com>
2018-01-24 14:09:01 +0000

    removed mapping as porting to another repo

--- CSharpCode/Mapping/MapZoomInformation/ZoomLevelCalculator/IZoomLevelCalculator.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace CwCodeLib.Mapping.MapZoomInformation.ZoomLevelCalculator
-{
-    internal interface IZoomLevelCalculator
-    {
-        int GetZoomLevel(BoundingBox boundingBox, int viewingWidthPx);
-    }
-}
